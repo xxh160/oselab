7d7f76378c1ddf4c4b6e72bbaf095929c292f937
191250045-Hou Weidong <191250045@smail.nju.edu.cn>
2021-12-02 00:07:17 +0800

    update

--- lab3/rawio/include/proc.h
+++ lab3/rawio/include/proc.h
@@ -50,8 +50,7 @@ typedef struct s_task {
 
 /* Number of tasks & procs */
 #define NR_TASKS	1
-// hwd: only one process
-#define NR_PROCS	1
+#define NR_PROCS	3
 
 /* stacks of tasks */
 #define STACK_SIZE_TTY		0x8000
@@ -60,7 +59,7 @@ typedef struct s_task {
 #define STACK_SIZE_TESTC	0x8000
 
 #define STACK_SIZE_TOTAL	(STACK_SIZE_TTY + \
-				STACK_SIZE_TESTA )
-				// STACK_SIZE_TESTB + \
-				// STACK_SIZE_TESTC)
+				STACK_SIZE_TESTA + \
+				STACK_SIZE_TESTB + \
+				STACK_SIZE_TESTC)
 
